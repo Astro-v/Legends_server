27409484008650d61c578d2a5c122b672284cc7e
Astro-v <valentinmichel07200@gmail.com>
2021-11-03 14:55:20 +0100

    commit

--- Map.hpp
+++ Map.hpp
@@ -6,6 +6,7 @@
 #include <string>
 
 /*---- LIBRARY SFML ----*/
+#include "SFML/Network.hpp"
 
 /*---- LOCAL FILE ----*/
 #include "Tile.hpp"
@@ -19,18 +20,24 @@ class Map {
     ~Map();
 
     /*---- INITIALIZE ----*/
-    void loadMap(std::string const& path);  // Load the map
+    void loadMap(std::string const& path);   // Load the map
+
+    /*---- COMMUNICATION ----*/
+    void receive();
+    void addPlayer(Player* player);    // Add the player and send to this player the data about the map
 
     protected:
     
     private:
-    std::vector<*Player> _listPlayer;       // List all player on the map
-    int _numberPlayer;                      // Number of player on the map
-    std::vector<TILE> _tile;                // Table of all tiles
+    std::vector<Player *> _players;          // List all player on the map
+    int _numberPlayers;                      // Number of player on the map
+    std::vector<TILE> _tile;                 // Table of all tiles
 
-    int _posX;                              // Position of the map
-    int _posY;                              // Position of the map
-    int _posZ;                              // Altitude of the map (<0 for underground maps)
+    std::vector<Map *>  _neighboor;          //
+
+    sf::Uint32 _posX;                        // Position of the map
+    sf::Uint32 _posY;                        // Position of the map
+    sf::Uint32 _posZ;                        // Altitude of the map (<0 for underground maps)
 }
 
 #endif // __MAP__
